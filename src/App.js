880e25adba5603978a26ea3c72e4eb5ba8d3575f
sukileung423 <sukileung423@gmail.com>
2022-11-10 10:06:04 +0800

    add function for fetching

--- src/App.js
+++ src/App.js
@@ -6,41 +6,44 @@ const App = () => {
     const [data, setData] = useState();
 
     // Fetching data from API and save to data
-    // @todo would using async help?
     useEffect(() => {
-        try {
-            fetch('/get')
-                .then((res) => res.json())
-                .then((data) => {
-                    setData(data.output);
-                })
+
+        const fetchData = async () => {
+            const [table] = await Promise.all([fetchTable()]);
+            setData(table)
         }
-        catch (e) {
-            console.log(e)
-        }
+
+        fetchData()
+
     }, [])
+
+    const fetchTable = async () => {
+        return fetch('/get')
+        .then((res) => res.json())
+        .then((data) => {
+            return data.output;
+        })
+    }
 
     // empty arr when not finished fetched
     let keys = !data ? [] : Object.keys(data[0]);
-
+    
 
     // function of creating header of table
     const header = (keys) => {
         return keys.map((key, index) => {
             return <th key={index} >{key}</th>
         })
-
     }
 
     // function of creating data rows
     const row = (keys, json, index) => {
         return (<tr key={index} >
             {keys.map((key, index) => {
-                return (<td key={index} >{json[key] ? json[key] : 'NULL'}</td>)
+                return (<td key={index} >{json[key] ? json[key] : 'NULL'}</td>) // print NULL for NULL results
             })}
         </tr>)
     }
-
 
     // main return 
     return (<div className='App' >
